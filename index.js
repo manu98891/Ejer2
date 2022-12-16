55e3976a2c9adee4e868f8b6b18545a1d614a5a8
Manuel Eugenio Losada Clua <ManuelEugenio.LosadaClua@endava.com>
2022-12-16 16:23:32 -0300

    Segundo ejercicio

---
+++ index.js
@@ -0,0 +1,12 @@
+const datos = [
+    "Manuel",
+    24,
+    false,
+    new Date(20,04,1998),
+    {
+        titulo: "Mort",
+        autor: "Terry Pratchett",
+        fecha: new Date(12,10,1987),
+        url: "https://www.amazon.com/-/es/Terry-Pratchett/dp/0062225715"
+    },
+]
